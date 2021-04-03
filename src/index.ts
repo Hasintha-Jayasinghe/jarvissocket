cc702bd8485823209f69ee20f5b0d08501d8ab39
Hasintha Jayasinghe <hasinthajayasinghe07@gmail.com>
2021-04-03 23:46:25 +0530

    First commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const server = createServer();
+const io = new Server(server, {
+  cors: { origin: '*' },
+});
+
+const sleep = (ms: number) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+let lastId: string;
+
+io.on('connection', socket => {
+  socket.on('message', text => {
+    console.log(text);
+    const data = JSON.parse(text);
+    console.log('sending data to jarvis');
+    io.emit('jarvis-message', text);
+    lastId = String(data.id);
+    sleep(3000);
+  });
+});
+
+server.listen(5000, () =>
+  console.log('Server listening on ws://localhost:5000')
+);
